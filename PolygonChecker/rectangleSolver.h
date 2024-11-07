ab273ebb5eab332580a05464d12d7f90e4274417
Vishwa5607 <Vsivakumar0329@conestogac.on.ca>
2024-11-07 09:25:07 -0500

    Made a list of functions for the rectangleSolver

---
+++ PolygonChecker/rectangleSolver.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <stdbool.h>
+
+typedef struct point {
+	int x;
+	int y;
+} POINT;
+
+
+bool is_Rectangle(POINT* points);
+int get_Peram(POINT* arrOfPoints);
+int get_Area(POINT* arrOfPoints);
+int get_Distance(POINT pointA, POINT pointB);
+bool is_Orthogonal(POINT pointA, POINT pointB, POINT pointC);
+void Sort_Points(POINT* arrOfPoints);
+
+
+
