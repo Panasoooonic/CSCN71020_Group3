6f58664a3d57e7e4f1c6e5c8d39741f398586982
Vishwa5607 <Vsivakumar0329@conestogac.on.ca>
2024-11-12 15:40:13 -0500

    In this push I have added further functionality to the traingle solver function. This includes angle check and type of triangle outputs.

--- PolygonChecker/triangleSolver.c
+++ PolygonChecker/triangleSolver.c
@@ -1,24 +1,32 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <math.h>
 
 #include "triangleSolver.h"
 
-char* analyzeTriangle(int side1, int side2, int side3) {
-	char* result = "";
-	if (side1 <= 0 || side2 <= 0 || side3 <= 0) {
-		result = "Not a triangle";
-	}
-	else if (side1 == side2 && side1 == side3) {
-		result = "Equilateral triangle";
-	}
-	else if ((side1 == side2 && side1 != side3) || 
-		(side1 == side3 && side1 != side2))
-	{
-		result = "Isosceles triangle";
-	}
-	else {
-		result = "Scalene triangle";
-	}
+#define PI 3.14159265
 
-	return result;
+char* analyzeTriangle(int side1, int side2, int side3) {
+    char* result = "";
+    if (side1 <= 0 || side2 <= 0 || side3 <= 0) {
+        result = "Not a triangle";
+    }
+    else if (side1 + side2 <= side3 || side1 + side3 <= side2 || side2 + side3 <= side1) {
+        result = "Not a triangle";
+    }
+    else if (side1 == side2 && side1 == side3) {
+        result = "Equilateral triangle";
+        printf("The triangle is a perfect triangle.\n");
+        printf("All angles are 60 degrees.\n");
+    }
+    else if ((side1 == side2 && side1 != side3) || 
+             (side1 == side3 && side1 != side2) || 
+             (side2 == side3 && side2 != side1)) {
+        result = "Isosceles triangle";
+    }
+    else {
+        result = "Scalene triangle";
+    }
+
+    return result;
 }
--- PolygonChecker/triangleSolver.h
+++ PolygonChecker/triangleSolver.h
@@ -1,2 +1,6 @@
 #pragma once
+#ifndef TRIANGLE_SOLVER_H
+#define TRIANGLE_SOLVER_H 
 char* analyzeTriangle(int side1, int side2, int side3);
+
+#endif // !TRIANGLE_SOLVER_H
