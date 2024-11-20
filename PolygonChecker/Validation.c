04e49d3876accd1dbf9a24cc8edd36d40d146868
Michael Campbell <Mcampbell6091@conestogac.on.ca>
2024-11-20 16:36:12 -0500

    Add Validation Branch

--- PolygonChecker/Validation.c
+++ PolygonChecker/Validation.c
@@ -4,6 +4,15 @@
 #include "Validation.h"
 
 
+int Validation()	// primary Validation function
+{
+	char input[10];
+	scanf_s("%s", &input, 9);	// accept string input
+	int inputValue = atof(&input);	// convert to integer
+	return inputValue;
+}
+
+
 int ValidateStartInput()
 {
 	int intInputted = 1; // set to 1 so the script plays
@@ -11,83 +20,38 @@ int ValidateStartInput()
 	// Repeat untill correct input is put in
 	while (repeat)
 	{
-		char shapeChoice[10];
-		int inputValue;
-		printf_s("Enter number: ");
-		scanf_s("%s", &shapeChoice, 9);
-		int isDigit = sscanf(shapeChoice, "%d", &inputValue); // isDigit is for testing if value inputted can convert to integer or not. (1) - yes
+		int inputValue = Validation();
 
-		switch (isDigit)
+		if (inputValue == 0 || inputValue == 1 || inputValue == 2)
 		{
-		case 1:
-			if (inputValue == 0 || inputValue == 1 || inputValue == 2)
-			{
-				intInputted = inputValue;
-				repeat = false;
-			}
-			else
-			{
-				printf_s("Please select numbers 0 1 or 2");
-			}
-			break;
-		default:
-			printf_s("Please enter a numeric value\n");
-			break;
-		}
-	}
-	return intInputted;
-}
-
-int ValidateTriangleInput()
-{
-	int intInputted = 1; // set to 1 so the script plays
-	bool repeat = true;
-	// Repeat untill correct input is put in
-	while (repeat)
-	{
-		char input[10];
-		int inputValue;
-		printf_s("Enter the length of a side of the triangle: ");
-		scanf_s("%s", &input, 9);
-		int isDigit = sscanf(input, "%d", &inputValue);// isDigit is for testing if value inputted can convert to integer or not. (1) - yes
-
-		switch (isDigit)
-		{
-		case 1:
 			intInputted = inputValue;
 			repeat = false;
-			break;
-		default:
-			printf_s("Please enter a numeric value\n");
-			break;
+		}
+		else
+		{
+			printf_s("Please select numbers 0 1 or 2");
 		}
 	}
 	return intInputted;
 }
 
-
-
-int ValidateRectangleInput()
+int ValidateInput()
 {
 	int intInputted = 1;	// set to 1 so the script plays
 	bool repeat = true;
 	// Repeat untill correct input is put in
 	while (repeat)
 	{
-		char input[10];
-		int inputValue;
-		scanf_s("%s", &input, 9);
-		int isDigit = sscanf(input, "%d", &inputValue);// isDigit is for testing if value inputted can convert to integer or not. (1) - yes
+		int inputValue = Validation();
 
-		switch (isDigit)
+		if (inputValue >= 1)
 		{
-		case 1:
 			intInputted = inputValue;
 			repeat = false;
-			break;
-		default:
-			printf_s("Please enter a numeric value\n");
-			break;
+		}
+		else
+		{
+			printf("Please enter a numeric value or a value greater than or equal to 1: ");
 		}
 	}
 	return intInputted;
